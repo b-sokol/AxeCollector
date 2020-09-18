10a9b18f4e2ab2e75c422c069e79899c2855d3aa
sokolb <sokolb@mac.com>
2020-09-18 18:10:02 -0400

    remove unnecessary profile class

--- axecollector/main_app/models.py
+++ axecollector/main_app/models.py
@@ -13,12 +13,6 @@ SERVICES = (
   ('F', 'Fretwork'),
 )
 
-class Profile(models.Model):
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  first_name= models.CharField(max_length=50)
-  last_name= models.CharField(max_length=50)
-  email= models.CharField(max_length=50)
-
 class String(models.Model):
   brand = models.CharField(max_length=50)
   line = models.CharField(max_length=50)
